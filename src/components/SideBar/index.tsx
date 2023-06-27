fc408b9124266d5fbde198419c3efbc1b923d431
eric-moniz <ericmoniz@outlook.com.br>
2023-06-27 12:49:11 -0300

    Criando o elemento de navegação

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,5 +1,25 @@
 import { Container } from './styles'
 
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+
+import menuImg from '../../assets/menu.svg'
+
 export function SideBar() {
-  return <Container> SideBar </Container>
+  return (
+    <Container>
+      <button type='button'>
+        <img src={menuImg} alt='Abrir e fechar o menu lateral' />
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <a href='#'>
+              <BurgerIcon />
+              <span>Hambúrgueres</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  )
 }
