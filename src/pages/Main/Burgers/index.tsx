75d120c8bf1f57992b2662c50f2bff981b4b6d97
eric-moniz <ericmoniz@outlook.com.br>
2023-06-30 06:34:03 -0300

    Construindo feed de alimentos

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -4,6 +4,26 @@ import { SnackTitle } from '../../../components/SnackTitle'
 import {} from './styles'
 
 export default function Burgers() {
+  const data = [
+    {
+      id: 1,
+      snack: 'burger',
+      name: 'Mega',
+      description:
+        'O artesanal tamanhp família recheado com três carnes suculentas, queijo e bacon.',
+      price: 25.5,
+      image: 'https://i.imgur.com/upjIUnG.jpg',
+    },
+    {
+      id: 2,
+      snack: 'burger',
+      name: 'Extra bacon',
+      description:
+        'Criado para os amantes de bacon, possui em todas as suas camadas bacon bem assado e ainda queijo e carne.',
+      price: 23.5,
+      image: 'https://i.imgur.com/B4J04AJ.jpg',
+    },
+  ]
   return (
     <>
       <Head title='Hambúrgueres' />
