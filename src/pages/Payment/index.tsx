2dcac0d851d37e7fba831464ee85518dfd7ff1ff
eric-moniz <ericmoniz@outlook.com.br>
2023-07-11 13:08:30 -0300

    Criando as bases para o formulário

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,13 +1,59 @@
 import { Head } from '../../components/Head'
 import { OrderHeader } from '../../components/OrderHeader'
-import { Container, Inner } from './styles'
+import { Container, Form, Inner } from './styles'
 
 export default function Payment() {
   return (
     <Container>
       <Head title='Pagamento' />
       <OrderHeader />
-      <Inner />
+      <Inner>
+        <Form>
+          <h4>Informações Pessoais</h4>
+
+          <div className='field'>
+            <label htmlFor='full-name'>Nome e sobrenome</label>
+            <input
+              type='text'
+              name='full-name'
+              id='full-name'
+              autoComplete='name'
+            />
+          </div>
+
+          <div className='grouped'>
+            <div className='field'>
+              <label htmlFor='email'>Email</label>
+              <input
+                type='email'
+                name='email'
+                id='email'
+                autoComplete='email'
+              />
+            </div>
+
+            <div className='field'>
+              <label htmlFor='mobile'>Celular</label>
+              <input
+                type='tel'
+                name='mobile'
+                id='mobile'
+                autoComplete='phone'
+              />
+            </div>
+
+            <div className='field'>
+              <label htmlFor='document'>CPF / CNPJ</label>
+              <input
+                type='text'
+                name='document'
+                id='document'
+                autoComplete='name'
+              />
+            </div>
+          </div>
+        </Form>
+      </Inner>
     </Container>
   )
 }
