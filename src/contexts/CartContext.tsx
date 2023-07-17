fb08e242daae22e1e7d9c5596f360288ba9642c1
eric-moniz <ericmoniz@outlook.com.br>
2023-07-17 13:15:56 -0300

    Recuperando Cart do LocalStorage

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -32,7 +32,13 @@ const localStorageKey = '@FoodCommerce:cart'
 
 export function CartProvider({ children }: CartProviderProps) {
   const navigate = useNavigate()
-  const [cart, setCart] = useState<Snack[]>([])
+  const [cart, setCart] = useState<Snack[]>(() => {
+    const value = localStorage.getItem(localStorageKey)
+
+    if (value) return JSON.parse(value)
+
+    return []
+  })
 
   function saveCart(items: Snack[]) {
     setCart(items)
