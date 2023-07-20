65dc660465ae8dec9a7188c749ed8342efd0478b
eric-moniz <ericmoniz@outlook.com>
2023-07-20 20:54:53 -0300

    Criando o endpoint Snacks

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,38 @@
-import dotenv from 'dotenv'
-import express, { Express, Request, Response } from 'express'
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
 
-dotenv.config()
+dotenv.config();
 
-const app: Express = express()
-const port = process.env.PORT || 5005
+const app: Express = express();
+const port = process.env.PORT || 5000;
+const prisma = new PrismaClient();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  const { message } = req.body
+app.get("/snacks", async (req: Request, res: Response) => {
+  const { snack } = req.query;
 
-  if (!message) return res.status(400).send({ error: 'Message is required' })
+  if (!snack) return res.status(400).send({ error: "Snack is required" });
 
-  res.send({ message })
-})
+  const snacks = await prisma.snack.findMany({
+    where: {
+      snack: {
+        equals: snack as string,
+      },
+    },
+  });
+  res.send({ snacks });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  const { message } = req.body;
+
+  if (!message) return res.status(400).send({ error: "Message is required" });
+
+  res.send({ message });
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}.`)
-})
+  console.log(`Server running on port ${port}.`);
+});
